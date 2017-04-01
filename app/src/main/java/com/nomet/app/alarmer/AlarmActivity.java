99e70e117b82fb5ee1cd7883960db71fb1b8c039
Владислав <vlad-s-d@ya.ru>
2017-04-01 13:36:35 +0300

    Затестил. Оно работает но странно. На Телефоне срабатывает с задержкой в 3 минуты. На планшете и эмуляторе с задержкой в 30 сек. Реализую возможность посмотреть время назначенное на будильник через переменную из другого activity

--- app/src/main/java/com/nomet/app/alarmer/AlarmActivity.java
+++ app/src/main/java/com/nomet/app/alarmer/AlarmActivity.java
@@ -2,6 +2,7 @@ package com.nomet.app.alarmer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AlarmActivity extends AppCompatActivity {
@@ -11,5 +12,8 @@ public class AlarmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
         Toast.makeText(this, "kekkkekeke", Toast.LENGTH_SHORT).show();
+        TextView text = (TextView)findViewById(R.id.itwork);
+
+        text.setText("Сработало");
     }
 }
--- app/src/main/java/com/nomet/app/alarmer/MainActivity.java
+++ app/src/main/java/com/nomet/app/alarmer/MainActivity.java
@@ -10,43 +10,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    public int hour;
+    public int minute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button set = (Button)findViewById(R.id.set);
         final TimePicker time = (TimePicker)findViewById(R.id.timePicker);
-        Calendar cal = new GregorianCalendar();
+        final Calendar cal = new GregorianCalendar();
         Calendar cur_cal = new GregorianCalendar();
         cur_cal.setTimeInMillis(System.currentTimeMillis());
         cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR));
-        cal.set(Calendar.HOUR_OF_DAY, 1);
-        cal.set(Calendar.MINUTE, 22);
+
         cal.set(Calendar.SECOND, cur_cal.get(Calendar.SECOND));
         cal.set(Calendar.MILLISECOND, cur_cal.get(Calendar.MILLISECOND));
         cal.set(Calendar.DATE, cur_cal.get(Calendar.DATE));
         cal.set(Calendar.MONTH, cur_cal.get(Calendar.MONTH));
 
-        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        final AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, AlarmActivity.class);
-        PendingIntent pintent = PendingIntent.getActivity(this, 0, intent, 0);
+        final PendingIntent pintent = PendingIntent.getActivity(this, 0, intent, 0);
         //alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 0, pintent);
-        alarm.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pintent);
+
 
         set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Setted", Toast.LENGTH_SHORT).show();
                 int h = time.getCurrentHour();
                 int m = time.getCurrentMinute();
+                cal.set(Calendar.HOUR_OF_DAY, h);
+                hour =h;
+                minute = m;
+                cal.set(Calendar.MINUTE, m);
+                alarm.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pintent);
             }
         });
+
     }
 
 }
