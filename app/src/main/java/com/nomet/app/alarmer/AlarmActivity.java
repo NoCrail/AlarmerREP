e467db83fa1c3c1f3c466d65b850a237dfb65670
Владислав <vlad-s-d@ya.ru>
2017-04-09 19:09:14 +0300

    Пробую дописать включение экрана при срабатывании будильника

--- app/src/main/java/com/nomet/app/alarmer/AlarmActivity.java
+++ app/src/main/java/com/nomet/app/alarmer/AlarmActivity.java
@@ -1,7 +1,11 @@
 package com.nomet.app.alarmer;
 
+import android.content.Context;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,6 +14,12 @@ public class AlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "My Tag");
+        mWakeLock.acquire();
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         setContentView(R.layout.activity_alarm);
         Toast.makeText(this, "kekkkekeke", Toast.LENGTH_SHORT).show();
         TextView text = (TextView)findViewById(R.id.itwork);
