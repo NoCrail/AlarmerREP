99c4bf8ac2b944e5843bfabbb90e91d1ece3cc4c
Владислав <vlad-s-d@ya.ru>
2017-04-20 14:57:30 +0300

    Полностью рабочий код будильника с включаемым экраном. Не теститлось на андроид 6

--- app/src/main/java/com/nomet/app/alarmer/AlarmActivity.java
+++ app/src/main/java/com/nomet/app/alarmer/AlarmActivity.java
@@ -1,7 +1,9 @@
 package com.nomet.app.alarmer;
 
+import android.Manifest;
 import android.content.Context;
 import android.os.PowerManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -14,13 +16,17 @@ public class AlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+        ContextCompat.checkSelfPermission(this, Manifest.permission.WAKE_LOCK);
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         PowerManager.WakeLock mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "My Tag");
+
         mWakeLock.acquire();
         Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        setContentView(R.layout.activity_alarm);
+
         Toast.makeText(this, "kekkkekeke", Toast.LENGTH_SHORT).show();
         TextView text = (TextView)findViewById(R.id.itwork);
 
